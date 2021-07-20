b37c1e8eac41ac8cc61fa1e5d8fb431be842a1ae
Jacky <jacky-943572677@qq.com>
2021-07-20 20:01:20 +0800

    build frontend at local

--- install.sh
+++ install.sh
@@ -7,15 +7,6 @@ echo "Copyright (c) 0xJacky 2021"
 echo
 echo "=========================="
 
-echo "installing yarn..."
-npm install -g yarn
-
-echo "Compiling frontend..."
-cd nginx-ui-frontend || exit 1
-yarn build
-
-cd ..
-
 echo "Compiling api server..."
 cd server || exit 1
 go build -o nginx-ui-server main.go
