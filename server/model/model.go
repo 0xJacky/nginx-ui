865d2ceaa47652937d032d66c066fabd8371ea42
0xJacky <me@jackyu.cn>
2023-04-13 15:40:35 +0800

    fix: soft delete

--- server/model/model.go
+++ server/model/model.go
@@ -17,10 +17,10 @@ import (
 var db *gorm.DB
 
 type Model struct {
-	ID        int        `gorm:"primary_key" json:"id"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `gorm:"index" json:"deleted_at"`
+	ID        int             `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time       `json:"created_at"`
+	UpdatedAt time.Time       `json:"updated_at"`
+	DeletedAt *gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
 
 func GenerateAllModel() []any {
