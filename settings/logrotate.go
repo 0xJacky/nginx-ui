b64f71e13afc14e2a9fd96fbcb13e72df1819b42
Jacky <me@jackyu.cn>
2024-10-05 20:02:15 +0800

    fix: typo error

--- settings/logrotate.go
+++ settings/logrotate.go
@@ -1,13 +1,13 @@
 package settings
 
 type Logrotate struct {
-	Enabled  bool   `json:"enabled"`
-	CMD      string `json:"cmd" protect:"true"`
-	Interval int    `json:"interval"`
+    Enabled  bool   `json:"enabled"`
+    CMD      string `json:"cmd" protected:"true"`
+    Interval int    `json:"interval"`
 }
 
 var LogrotateSettings = Logrotate{
-	Enabled:  false,
-	CMD:      "logrotate /etc/logrotate.d/nginx",
-	Interval: 1440, // 24 hours
+    Enabled:  false,
+    CMD:      "logrotate /etc/logrotate.d/nginx",
+    Interval: 1440, // 24 hours
 }
