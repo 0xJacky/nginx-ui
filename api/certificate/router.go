36221937bf16ec66e572a38f6ad36041cb7ce0a6
Jacky <me@jackyu.cn>
2024-10-28 17:03:59 +0800

    fix: some certificate routes are incorrect #669

--- api/certificate/router.go
+++ api/certificate/router.go
@@ -4,18 +4,18 @@ import "github.com/gin-gonic/gin"
 
 func InitDNSCredentialRouter(r *gin.RouterGroup) {
 	r.GET("dns_credentials", GetDnsCredentialList)
-	r.GET("dns_credential/:id", GetDnsCredential)
-	r.POST("dns_credential", AddDnsCredential)
-	r.POST("dns_credential/:id", EditDnsCredential)
-	r.DELETE("dns_credential/:id", DeleteDnsCredential)
+	r.GET("dns_credentials/:id", GetDnsCredential)
+	r.POST("dns_credentials", AddDnsCredential)
+	r.POST("dns_credentials/:id", EditDnsCredential)
+	r.DELETE("dns_credentials/:id", DeleteDnsCredential)
 }
 
 func InitCertificateRouter(r *gin.RouterGroup) {
 	r.GET("certs", GetCertList)
-	r.GET("cert/:id", GetCert)
-	r.POST("cert", AddCert)
-	r.POST("cert/:id", ModifyCert)
-	r.DELETE("cert/:id", RemoveCert)
+	r.GET("certs/:id", GetCert)
+	r.POST("certs", AddCert)
+	r.POST("certs/:id", ModifyCert)
+	r.DELETE("certs/:id", RemoveCert)
 	r.PUT("cert_sync", SyncCertificate)
 	r.GET("certificate/dns_providers", GetDNSProvidersList)
 	r.GET("certificate/dns_provider/:code", GetDNSProvider)
@@ -27,10 +27,10 @@ func InitCertificateWebSocketRouter(r *gin.RouterGroup) {
 
 func InitAcmeUserRouter(r *gin.RouterGroup) {
 	r.GET("acme_users", GetAcmeUserList)
-	r.GET("acme_user/:id", GetAcmeUser)
-	r.POST("acme_user", CreateAcmeUser)
-	r.POST("acme_user/:id", ModifyAcmeUser)
-	r.POST("acme_user/:id/register", RegisterAcmeUser)
-	r.DELETE("acme_user/:id", DestroyAcmeUser)
-	r.PATCH("acme_user/:id", RecoverAcmeUser)
+	r.GET("acme_users/:id", GetAcmeUser)
+	r.POST("acme_users", CreateAcmeUser)
+	r.POST("acme_users/:id", ModifyAcmeUser)
+	r.POST("acme_users/:id/register", RegisterAcmeUser)
+	r.DELETE("acme_users/:id", DestroyAcmeUser)
+	r.PATCH("acme_users/:id", RecoverAcmeUser)
 }
