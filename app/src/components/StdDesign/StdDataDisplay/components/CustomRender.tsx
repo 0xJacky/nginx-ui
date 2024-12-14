5252f14316a0935f135917cf1d9ef30789907a5a
Jacky <me@jackyu.cn>
2024-12-14 17:06:08 +0800

    fix(lint): rm sonarjs/no-redeclare comment

--- app/src/components/StdDesign/StdDataDisplay/components/CustomRender.tsx
+++ app/src/components/StdDesign/StdDataDisplay/components/CustomRender.tsx
@@ -1,7 +1,7 @@
 import type { CustomRender } from '@/components/StdDesign/StdDataDisplay/StdTableTransformer'
 import _ from 'lodash'
 
-// eslint-disable-next-line ts/no-redeclare,sonarjs/no-redeclare
+// eslint-disable-next-line ts/no-redeclare
 export function CustomRender(props: CustomRender) {
   return props.column.customRender
     ? props.column.customRender(props)
