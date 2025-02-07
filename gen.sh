7bb8cb5d197523a8fe32d8fc738f6ab8699269bc
Jacky <me@jackyu.cn>
2025-02-07 22:27:38 +0800

    chore: update error definations

--- gen.sh
+++ gen.sh
@@ -4,7 +4,7 @@ go run generate.go -config ../../app.ini
 popd || exit
 
 # generate error definitions
-go run cmd/errdef/generate.go . ts ./app/src/constants/errors
+go run cmd/errdef/generate.go -project . -type ts -output ./app/src/constants/errors -ignore-dirs .devcontainer,app,.github
 
 # parse nginx directive indexs
 go run cmd/ngx_dir_index/ngx_dir_index.go ./internal/nginx/nginx_directives.json
