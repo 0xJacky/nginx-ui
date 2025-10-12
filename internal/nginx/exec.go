bc3ef78e4278ffa4e2ce93175f56061fa5248fe6
Alireza Gholie <alireza.gholie@gmail.com>
2025-10-12 06:51:43 +0330

    fix: support Windows in execShell by replacing /bin/sh with cmd.exe

--- internal/nginx/exec.go
+++ internal/nginx/exec.go
@@ -3,13 +3,28 @@ package nginx
 import (
 	"context"
 	"os/exec"
+	"runtime"
 
 	"github.com/0xJacky/Nginx-UI/internal/docker"
 	"github.com/0xJacky/Nginx-UI/settings"
 )
 
 func execShell(cmd string) (stdOut string, stdErr error) {
-	return execCommand("/bin/sh", "-c", cmd)
+	var execCmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		execCmd = exec.Command("cmd", "/c", cmd)
+	} else {
+		execCmd = exec.Command("/bin/sh", "-c", cmd)
+	}
+
+	execCmd.Dir = GetNginxExeDir()
+	bytes, err := execCmd.CombinedOutput()
+	stdOut = string(bytes)
+	if err != nil {
+		stdErr = err
+	}
+	return
 }
 
 func execCommand(name string, cmd ...string) (stdOut string, stdErr error) {
