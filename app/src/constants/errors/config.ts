30908f3c28d777c0be7cd2611f7d29145ff76752
0xJacky <me@jackyu.cn>
2026-04-27 12:22:46 +0800

    chore: prepare v2.3.9

--- app/src/constants/errors/config.ts
+++ app/src/constants/errors/config.ts
@@ -1,4 +1,8 @@
 export default {
+  40014: () => $gettext('File name is not allowed: {0}'),
+  40015: () => $gettext('File content must be valid UTF-8 text'),
+  40016: () => $gettext('File content contains invalid control characters'),
+  40017: () => $gettext('File content contains restricted nginx directive: {0}'),
   50006: () => $gettext('Path: {0} is not under the nginx conf dir: {1}'),
   50007: () => $gettext('Destination file: {0} already exists'),
   50008: () => $gettext('Nginx test failed: {0}'),
