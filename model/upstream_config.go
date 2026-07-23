2cb7ee9102c9d87274de2fa104db804841d140a0
milkfish <i@mxd.moe>
2026-07-23 19:28:38 +0800

    fix(model): remove gorm default:true from UpstreamConfig.Enabled (#1758)

--- model/upstream_config.go
+++ model/upstream_config.go
@@ -3,5 +3,5 @@ package model
 type UpstreamConfig struct {
 	Model
 	Socket  string `json:"socket" gorm:"uniqueIndex"` // host:port address
-	Enabled bool   `json:"enabled" gorm:"default:true"`
+	Enabled bool   `json:"enabled"`
 }
